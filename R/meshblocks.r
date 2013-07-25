78c3fe4496828f21fb47508f3f1f0cdd293c1783
Duncan Garmonsway <nacnudus@gmail.com>
2013-07-25 06:51:57 +0000

    A-Z plots

--- R/meshblocks.r
+++ R/meshblocks.r
@@ -3,7 +3,7 @@ require(rgdal)
 require(maptools)
 require(mapproj)
 require(plyr)
-require(ggplot2)
+require(RColorBrewer)
 
 # Global
 setwd("/home/nacnudus/R/ruralRoads")
@@ -12,7 +12,7 @@ setwd("/home/nacnudus/R/ruralRoads")
 ######
 
 # Coastline for context and because police boundaries exceed it
-coast <- readOGR("data/Coastline/", "nz-mainland-coastlines-to")
+coast <- readOGR("data/coastlineLine/", "nz-mainland-coastlines-to")
 
 # Districts, Areas, Stations
 districts <- readOGR("data/PoliceBoundaries/nz-police-district-bounda/", "nz-police-district-bounda")
@@ -29,11 +29,22 @@ concordance <- read.csv(
 # Clean
 #######
 
-# convert coast from spatialLines to spatialPolygons
-coastPolySet <- SpatialLines2PolySet(coast)
-coastPolygons <- PolySet2SpatialPolygons(coastPolySet)
-proj4string(coastPolygons) <- CRS(proj4string(coast))
-# not pretty and won't work.
+# # simplify the coast polygons
+# coast2 <- gSimplify(coast, 0.4, topologyPreserve=TRUE)
+# areas <- lapply(coast2@polygons, function(x) sapply(x@Polygons, function(y) y@area))
+# bigpolys <- lapply(areas, function(x) which(x > 0.1))
+# for(i in 1:length(bigpolys)){
+#   if(length(bigpolys[[i]]) >= 1 && bigpolys[[i]][1] >= 1){
+#     coast2@polygons[[i]]@Polygons <- coast2@polygons[[i]]@Polygons[bigpolys[[i]]]
+#     coast2@polygons[[i]]@plotOrder <- 1:length(coast2@polygons[[i]]@Polygons)
+#   }
+# }
+
+# # convert coast from spatialLines to spatialPolygons
+# coastPolySet <- SpatialLines2PolySet(coast)
+# coastPolygons <- PolySet2SpatialPolygons(coastPolySet)
+# proj4string(coastPolygons) <- CRS(proj4string(coast))
+# # not pretty and won't work.
 
 colnames(concordance) <- c("MB06", "urban.rural", "main.urban.area")
 # later, "MB06" has to be "id" so it can be joined to the polygons.
@@ -93,72 +104,98 @@ meshblocks@data <- join(meshblocks@data, area.aggregation, by = "MB06")
 # first, code the classifications A-G and Z
 urban.rural <- data.frame(urban.rural = levels(meshblocks@data$urban.rural), code = c("Z", "G", "B", "A", "D", "F", "E", "C"))
 meshblocks@data <- join(meshblocks@data, urban.rural, by = "urban.rural")
-head(meshblocks@data)
-# subset by urban.rural
-meshblocksA <- subset(meshblocks, meshblocks@data$code == "A")
-meshblocksB <- subset(meshblocks, meshblocks@data$code == "B")
-meshblocksC <- subset(meshblocks, meshblocks@data$code == "C")
-meshblocksD <- subset(meshblocks, meshblocks@data$code == "D")
-meshblocksE <- subset(meshblocks, meshblocks@data$code == "E")
-meshblocksF <- subset(meshblocks, meshblocks@data$code == "F")
-meshblocksG <- subset(meshblocks, meshblocks@data$code == "G")
-meshblocksZ <- subset(meshblocks, meshblocks@data$code == "Z")
-# union each one
-meshblocksAunion <- unionSpatialPolygons(meshblocksA, meshblocksA@data$code)
-meshblocksBunion <- unionSpatialPolygons(meshblocksB, meshblocksB@data$code)
-meshblocksCunion <- unionSpatialPolygons(meshblocksC, meshblocksC@data$code)
-meshblocksDunion <- unionSpatialPolygons(meshblocksD, meshblocksD@data$code)
-meshblocksEunion <- unionSpatialPolygons(meshblocksE, meshblocksE@data$code)
-meshblocksFunion <- unionSpatialPolygons(meshblocksF, meshblocksF@data$code)
-meshblocksGunion <- unionSpatialPolygons(meshblocksG, meshblocksG@data$code)
-meshblocksZunion <- unionSpatialPolygons(meshblocksZ, meshblocksZ@data$code)
+
+# subset by urban.rural (gBuffer prevents non-noded intersections error)
+meshblocksA <- gBuffer(subset(meshblocks, meshblocks@data$code == "A"), width=0, byid=TRUE)
+meshblocksB <- gBuffer(subset(meshblocks, meshblocks@data$code == "B"), width=0, byid=TRUE)
+meshblocksC <- gBuffer(subset(meshblocks, meshblocks@data$code == "C"), width=0, byid=TRUE)
+meshblocksD <- gBuffer(subset(meshblocks, meshblocks@data$code == "D"), width=0, byid=TRUE)
+meshblocksE <- gBuffer(subset(meshblocks, meshblocks@data$code == "E"), width=0, byid=TRUE)
+meshblocksF <- gBuffer(subset(meshblocks, meshblocks@data$code == "F"), width=0, byid=TRUE)
+meshblocksG <- gBuffer(subset(meshblocks, meshblocks@data$code == "G"), width=0, byid=TRUE)
+meshblocksZ <- gBuffer(subset(meshblocks, meshblocks@data$code == "Z"), width=0, byid=TRUE)
+# union all polygons in each one
+meshblocksAunion <- unionSpatialPolygons(meshblocksA, rep(1, length(meshblocksA@polygons)))
+meshblocksBunion <- unionSpatialPolygons(meshblocksB, rep(1, length(meshblocksB@polygons)))
+meshblocksCunion <- unionSpatialPolygons(meshblocksC, rep(1, length(meshblocksC@polygons)))
+meshblocksDunion <- unionSpatialPolygons(meshblocksD, rep(1, length(meshblocksD@polygons)))
+meshblocksEunion <- unionSpatialPolygons(meshblocksE, rep(1, length(meshblocksE@polygons)))
+meshblocksFunion <- unionSpatialPolygons(meshblocksF, rep(1, length(meshblocksF@polygons)))
+meshblocksGunion <- unionSpatialPolygons(meshblocksG, rep(1, length(meshblocksG@polygons))) # still some
+  # error but doesn't matter because is never plotted (most rural - plot by elimination)
+meshblocksZunion <- unionSpatialPolygons(meshblocksZ, rep(1, length(meshblocksZ@polygons)))
 
 # Do
 ####
 
 ur.legend <- ddply(urban.rural, .(code), function(x) (paste(as.character(x$code), as.character(x$urban.rural))))$V1
+fillcolours <- brewer.pal(12, "Set3")
 
 # plot A=Urban
-plot(districts, col = districts@data$DISTRICT_N, lwd = 0.2)
+png("plots/A.png", width=420, height=594, units="mm", res=600) # A3 portrait
+plot(districts, col = fillcolours[districts@data$DISTRICT_N], lwd = 0.2)
 plot(meshblocksAunion, col = "white", lwd = 0.2, add = TRUE)
-plot(coast, lwd = 0.2, col = "white", add = TRUE)
+plot(coast, lwd = 0.2, col = "black", add = TRUE)
 title(main = "A = Urban")
 legend("bottomright", legend = ur.legend, cex = 0.75)
+dev.off()
 
 # plot AB=Urban
-plot(districts, col = districts@data$DISTRICT_N, lwd = 0.2)
+png("plots/AB.png", width=420, height=594, units="mm", res=600) # A3 portrait
+plot(districts, col = fillcolours[districts@data$DISTRICT_N], lwd = 0.2)
 plot(meshblocksAunion, col = "white", lwd = 0.2, add = TRUE)
 plot(meshblocksBunion, col = "white", lwd = 0.2, add = TRUE)
-plot(coast, lwd = 0.2, col = "white", add = TRUE)
+plot(coast, lwd = 0.2, col = "black", add = TRUE)
 title(main = "AB = Urban")
 legend("bottomright", legend = ur.legend, cex = 0.75)
+dev.off()
 
 # plot ABC=Urban
-plot(districts, col = districts@data$DISTRICT_N, lwd = 0.2)
+png("plots/ABC.png", width=420, height=594, units="mm", res=600) # A3 portrait
+plot(districts, col = fillcolours[districts@data$DISTRICT_N], lwd = 0.2)
 plot(meshblocksAunion, col = "white", lwd = 0.2, add = TRUE)
 plot(meshblocksBunion, col = "white", lwd = 0.2, add = TRUE)
 plot(meshblocksCunion, col = "white", lwd = 0.2, add = TRUE)
-plot(coast, lwd = 0.2, col = "white", add = TRUE)
+plot(coast, lwd = 0.2, col = "black", add = TRUE)
 title(main = "ABC = Urban")
 legend("bottomright", legend = ur.legend, cex = 0.75)
+dev.off()
 
 # plot ABCD=Urban
-plot(districts, col = districts@data$DISTRICT_N, lwd = 0.2)
+png("plots/ABCD.png", width=420, height=594, units="mm", res=600) # A3 portrait
+plot(districts, col = fillcolours[districts@data$DISTRICT_N], lwd = 0.2)
 plot(meshblocksAunion, col = "white", lwd = 0.2, add = TRUE)
 plot(meshblocksBunion, col = "white", lwd = 0.2, add = TRUE)
 plot(meshblocksCunion, col = "white", lwd = 0.2, add = TRUE)
 plot(meshblocksDunion, col = "white", lwd = 0.2, add = TRUE)
-plot(coast, lwd = 0.2, col = "white", add = TRUE)
+plot(coast, lwd = 0.2, col = "black", add = TRUE)
 title(main = "ABCD = Urban")
 legend("bottomright", legend = ur.legend, cex = 0.75)
+dev.off()
 
 # plot ABCDE=Urban
-plot(districts, col = districts@data$DISTRICT_N, lwd = 0.2)
+png("plots/ABCDE.png", width=420, height=594, units="mm", res=600) # A3 portrait
+plot(districts, col = fillcolours[districts@data$DISTRICT_N], lwd = 0.2)
 plot(meshblocksAunion, col = "white", lwd = 0.2, add = TRUE)
 plot(meshblocksBunion, col = "white", lwd = 0.2, add = TRUE)
 plot(meshblocksCunion, col = "white", lwd = 0.2, add = TRUE)
 plot(meshblocksDunion, col = "white", lwd = 0.2, add = TRUE)
 plot(meshblocksEunion, col = "white", lwd = 0.2, add = TRUE)
-plot(coast, lwd = 0.2, col = "white", add = TRUE)
+plot(coast, lwd = 0.2, col = "black", add = TRUE)
 title(main = "ABCDE = Urban")
 legend("bottomright", legend = ur.legend, cex = 0.75)
+dev.off()
+
+# plot ABCDEF=Urban
+png("plots/ABCDEF.png", width=420, height=594, units="mm", res=600) # A3 portrait
+plot(districts, col = fillcolours[districts@data$DISTRICT_N], lwd = 0.2)
+plot(meshblocksAunion, col = "white", lwd = 0.2, add = TRUE)
+plot(meshblocksBunion, col = "white", lwd = 0.2, add = TRUE)
+plot(meshblocksCunion, col = "white", lwd = 0.2, add = TRUE)
+plot(meshblocksDunion, col = "white", lwd = 0.2, add = TRUE)
+plot(meshblocksEunion, col = "white", lwd = 0.2, add = TRUE)
+plot(meshblocksFunion, col = "white", lwd = 0.2, add = TRUE)
+plot(coast, lwd = 0.2, col = "black", add = TRUE)
+title(main = "ABCDEF = Urban")
+legend("bottomright", legend = ur.legend, cex = 0.75)
+dev.off()
