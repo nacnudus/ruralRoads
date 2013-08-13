71276eb4af2ef33c6fc8a6fa4ed48488d49f96d6
Duncan Garmonsway <nacnudus@gmail.com>
2013-08-13 00:16:56 +0000

    classified crashes by urban/rural

--- 07-2013-575/R/01-load.r
+++ 07-2013-575/R/01-load.r
@@ -1,1 +1,10 @@
 # load the crashes
+crashes <- loadCrashes("data/BoP-coordinates.csv")
+
+# meshblock ID per crash ID ----------------------------------------------
+ID <- over(crashes , meshblocks)
+crashMeshblockID <- cbind(crashes@data$id, ID[, c("MB06", "urban.rural")])
+write.table(crashMeshblockID
+            , row.names = FALSE
+            , col.names = c("crashID", "meshblockID", "urbanRural")
+            , file = "output/crashMeshblockID.txt")
