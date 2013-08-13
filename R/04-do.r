934278877c6600f104d4293200367c59bfa90c44
Duncan Garmonsway <nacnudus@gmail.com>
2013-08-13 00:14:05 +0000

    typo

--- R/04-do.r
+++ R/04-do.r
@@ -37,7 +37,7 @@ qplot(easting, northing, data = as.data.frame(crashes))
 
 # meshblock ID per crash ID ----------------------------------------------
 ID <- over(crashes , meshblocks)
-crashMeshblockID <- cbind(crashes@data$id ,ID[, c("MB06", "urban.rural")])
+crashMeshblockID <- cbind(crashes@data$id, ID[, c("MB06", "urban.rural")])
 write.table(crashMeshblockID
             , row.names = FALSE
             , col.names = c("crashID", "meshblockID", "urbanRural")
