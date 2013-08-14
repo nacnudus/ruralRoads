0572c64d00a2c5052509dec2807b23e2591c7914
Duncan Garmonsway <nacnudus@gmail.com>
2013-08-14 08:42:52 +0000

    load crashMeshblockID.txt and meshblockUrbanRural.txt

--- 07-2013-575/R/01-load.r
+++ 07-2013-575/R/01-load.r
@@ -3,3 +3,10 @@ crashes <- read.csv("data/BoP-crashes.txt", header = FALSE)
 drivers <- read.csv("data/BoP-drivers.txt", header = FALSE)
 driversCauses <- read.csv("data/BoP-drivers-causes.txt", header = FALSE)
 victims <- read.csv("data/BoP-victims.txt", header = FALSE)
+
+crashMeshblocks <- read.table("output/crashMeshblockID.txt"
+                              , header = TRUE
+                              , quote = "\"")
+meshblockUrban <- read.table("../output/meshblockUrbanRural.txt"
+                             , header = TRUE
+                             , quote = "\"")
