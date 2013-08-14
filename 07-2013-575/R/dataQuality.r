361396d48ecfbfab98a15fd55bb31614b07eddb3
Duncan Garmonsway <nacnudus@gmail.com>
2013-08-14 08:13:08 +0000

    fixed to count only unique crashes in each table, and wrote an explanation at the top

--- 07-2013-575/R/dataQuality.r
+++ 07-2013-575/R/dataQuality.r
@@ -1,4 +1,9 @@
 # Investigates the CAS data quality, e.g. how many crashes have coordinates?
+# Note that only unique crashIDs are counted.  For example, the code
+# dataQuality(driversCauses.) takes unique crashIDs from the coordinates
+# table, and sees how many unique matching crashIDs there are in the crashes
+# table. You could say, for all those crashes that have at least one
+# driver cause, so many don't even have an entry in the coordinates table.
 
 source("R/01-load.r")
 source("R/02-clean.r")
@@ -26,7 +31,7 @@ dataQuality <- function(x) {
 # left join all the other tables onto coordinates to see how many matches there
 # are.
 
-coordinates. <- data.frame(crashID = coordinates@data[, "crashID"])
+coordinates. <- data.frame(crashID = unique(coordinates@data[, "crashID"]))
 coordinates. <- join(coordinates.
                      , data.frame(crashID = crashes$crashID
                                   , crashes = 1)
@@ -49,7 +54,7 @@ coordinates. <- join(coordinates.
 
 # left join all the other tables onto crashes to see how many matches there are
 
-crashes. <- data.frame(crashID = crashes[, "crashID"])
+crashes. <- data.frame(crashID = unique(crashes[, "crashID"]))
 crashes. <- join(crashes.
                      , data.frame(crashID = coordinates@data$crashID
                                   , coordinates = 1)
@@ -72,7 +77,7 @@ crashes. <- join(crashes.
 
 # left join all the other tables onto drivers to see how many matches there are
 
-drivers. <- data.frame(crashID = drivers[, "crashID"])
+drivers. <- data.frame(crashID = unique(drivers[, "crashID"]))
 drivers. <- join(drivers.
                  , data.frame(crashID = coordinates@data$crashID
                               , coordinates = 1)
@@ -95,7 +100,7 @@ drivers. <- join(drivers.
 
 # left join all the other tables onto victims to see how many matches there are
 
-victims. <- data.frame(crashID = victims[, "crashID"])
+victims. <- data.frame(crashID = unique(victims[, "crashID"]))
 victims. <- join(victims.
                  , data.frame(crashID = coordinates@data$crashID
                               , coordinates = 1)
@@ -119,7 +124,7 @@ victims. <- join(victims.
 # left join all the other tables onto driversCauses to see how many matches there
 # are
 
-driversCauses. <- data.frame(crashID = driversCauses[, "crashID"])
+driversCauses. <- data.frame(crashID = unique(driversCauses[, "crashID"]))
 driversCauses. <- join(driversCauses.
                  , data.frame(crashID = coordinates@data$crashID
                               , coordinates = 1)
