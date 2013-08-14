07e95b4c56044eba93ea4a1530992bb93e96cd3e
Duncan Garmonsway <nacnudus@gmail.com>
2013-08-14 07:58:20 +0000

    missing data quantified

--- 07-2013-575/R/dataQuality.r
+++ 07-2013-575/R/dataQuality.r
@@ -1,5 +1,8 @@
 # Investigates the CAS data quality, e.g. how many crashes have coordinates?
 
+source("R/01-load.r")
+source("R/02-clean.r")
+
 require(reshape2)
 
 
@@ -25,13 +28,16 @@ dataQuality <- function(x) {
 
 coordinates. <- data.frame(crashID = coordinates@data[, "crashID"])
 coordinates. <- join(coordinates.
-                     , data.frame(crashID = crashes$crashID, crashes = 1)
+                     , data.frame(crashID = crashes$crashID
+                                  , crashes = 1)
                      , match = "first")
 coordinates. <- join(coordinates.
-                     , data.frame(crashID = crashes$crashID, drivers = 1)
+                     , data.frame(crashID = drivers$crashID
+                                  , drivers = 1)
                      , match = "first")
 coordinates. <- join(coordinates.
-                     , data.frame(crashID = victims$crashID, victims = 1)
+                     , data.frame(crashID = victims$crashID
+                                  , victims = 1)
                      , match = "first")
 coordinates. <- join(coordinates.
                      , data.frame(crashID = driversCauses$crashID
@@ -45,15 +51,94 @@ coordinates. <- join(coordinates.
 
 crashes. <- data.frame(crashID = crashes[, "crashID"])
 crashes. <- join(crashes.
-                     , data.frame(crashID = crashes$crashID, crashes = 1)
+                     , data.frame(crashID = coordinates@data$crashID
+                                  , coordinates = 1)
                      , match = "first")
 crashes. <- join(crashes.
-                     , data.frame(crashID = crashes$crashID, drivers = 1)
+                     , data.frame(crashID = drivers$crashID
+                                  , drivers = 1)
                      , match = "first")
 crashes. <- join(crashes.
-                     , data.frame(crashID = victims$crashID, victims = 1)
+                     , data.frame(crashID = victims$crashID
+                                  , victims = 1)
                      , match = "first")
 crashes. <- join(crashes.
                      , data.frame(crashID = driversCauses$crashID
                                   , driversCauses = 1)
                      , match = "first")
+
+
+# drivers. ---------------------------------------------------------------
+
+# left join all the other tables onto drivers to see how many matches there are
+
+drivers. <- data.frame(crashID = drivers[, "crashID"])
+drivers. <- join(drivers.
+                 , data.frame(crashID = coordinates@data$crashID
+                              , coordinates = 1)
+                 , match = "first")
+drivers. <- join(drivers.
+                 , data.frame(crashID = crashes$crashID
+                              , crashes = 1)
+                 , match = "first")
+drivers. <- join(drivers.
+                 , data.frame(crashID = victims$crashID
+                              , victims = 1)
+                 , match = "first")
+drivers. <- join(drivers.
+                 , data.frame(crashID = driversCauses$crashID
+                              , driversCauses = 1)
+                 , match = "first")
+
+
+# victims. ---------------------------------------------------------------
+
+# left join all the other tables onto victims to see how many matches there are
+
+victims. <- data.frame(crashID = victims[, "crashID"])
+victims. <- join(victims.
+                 , data.frame(crashID = coordinates@data$crashID
+                              , coordinates = 1)
+                 , match = "first")
+victims. <- join(victims.
+                 , data.frame(crashID = crashes$crashID
+                              , crashes = 1)
+                 , match = "first")
+victims. <- join(victims.
+                 , data.frame(crashID = drivers$crashID
+                              , drivers = 1)
+                 , match = "first")
+victims. <- join(victims.
+                 , data.frame(crashID = driversCauses$crashID
+                              , driversCauses = 1)
+                 , match = "first")
+
+
+# driversCauses. ---------------------------------------------------------------
+
+# left join all the other tables onto driversCauses to see how many matches there
+# are
+
+driversCauses. <- data.frame(crashID = driversCauses[, "crashID"])
+driversCauses. <- join(driversCauses.
+                 , data.frame(crashID = coordinates@data$crashID
+                              , coordinates = 1)
+                 , match = "first")
+driversCauses. <- join(driversCauses.
+                 , data.frame(crashID = crashes$crashID
+                              , crashes = 1)
+                 , match = "first")
+driversCauses. <- join(driversCauses.
+                 , data.frame(crashID = drivers$crashID
+                              , drivers = 1)
+                 , match = "first")
+driversCauses. <- join(driversCauses.
+                 , data.frame(crashID = victims$crashID
+                              , victims = 1)
+                 , match = "first")
+
+dataQuality(coordinates.)
+dataQuality(crashes.)
+dataQuality(drivers.)
+dataQuality(victims.)
+dataQuality(driversCauses.)
