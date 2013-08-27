a5f3b3e2759f0160b05c5f077fa94abd3147be3e
Duncan Garmonsway <nacnudus@gmail.com>
2013-08-27 20:49:45 +0000

    knitDoc function and require pander for tables

--- 07-2013-575/R/01-function.r
+++ 07-2013-575/R/01-function.r
@@ -6,10 +6,17 @@ require(lubridate)
 require(reshape2)
 require(stringr)
 require(xtable)
+require(pander)
 
 source("../R/01-function.r")
 
 # function to facilitate subsetting of data frames by their column class
 colClass <- function(DF, colclasses= c("numeric", "integer")) {
   laply(DF, function(vec, test) (class(vec) %in% test), test=colclasses)
+}
+
+# function to write to .docx
+knitDoc <- function(name) {
+  knit(paste0(name, ".Rmd"), encoding = "utf-8")
+  system(paste0("pandoc -o ", name, ".docx ", name, ".md"))
 }
