ed76de329d2e6b1a3350582031d8178990561fef
Duncan Garmonsway <nacnudus@gmail.com>
2013-09-04 23:31:05 +0000

    prettier map for Saskia

--- 07-2013-575/R/Map.r
+++ 07-2013-575/R/Map.r
@@ -1,7 +1,16 @@
-load("../output/spatialData.Rdata")
+
+# function ----------------------------------------------------------------
+
 source("../R/01-function.r")
 
+
+# load --------------------------------------------------------------------
+
+load("../output/spatialData.Rdata")
 coordinates <- loadCrashes("data/BoP-coordinates.csv")
+
+
+# clean -------------------------------------------------------------------
 
 # subset for Bay of Plenty meshblocks
 meshblocksBoP <- subset(meshblocks, meshblocks@data$policeDistrict == "BAY OF PLENTY" & 
@@ -21,12 +30,15 @@ stationsBoP <- subset(stations, stations@data$DISTRICT_N == "Bay of Plenty")
 stationLabels <- as.data.frame(gCentroid(stationsBoP,byid=TRUE)@coords)
 stationLabels$label <- stationsBoP@data$STATION_NA
 
+
+# do ----------------------------------------------------------------------
+
 # plot and save
-png("plots/BayOfPlenty.png", width=10, height=10, units="in", res=600)
-plot(stationsBoP, lwd = 0.2, col = "lightgrey")
-plot(coastline, lwd = 0.2, add = TRUE)
+png("plots/BayOfPlenty.png", width=420/2, height=594/2, units="mm", res=600) # A4 portrait
+plot(stationsBoP, lwd = 0.4, col = "lightgrey")
+plot(coastline, lwd = 0.4, add = TRUE)
 plot(meshblocksBoP, lwd = 0.2, col = "lightblue", add = TRUE)
-plot(highways, lwd = 0.2, col = "red", add = TRUE)
-plot(coordinates, col = alpha("darkgreen", 0.2), pch = 16, cex = 0.5, add = TRUE)
-text(stationLabels$x, stationLabels$y, labels = stationLabels$label, cex = 0.2)
+plot(highways, lwd = 0.4, lty = 2, col = "blue", add = TRUE)
+plot(coordinates, col = alpha("red", 0.2), pch = 16, cex = 0.5, add = TRUE)
+text(stationLabels$x, stationLabels$y, labels = stationLabels$label, cex = 0.75)
 dev.off()
